6b49110c118c215dde42d0f1e73821a5f7d535d1
Niniburu <101002010+Niniburu@users.noreply.github.com>
2022-03-05 18:00:44 +0500

    Add files via upload

---
+++ backgrounds/background 7/script.js
@@ -0,0 +1,25 @@
+/*
+ * random() - Clubbing
+ *  
+ * I added the SCSS random() function to this
+ * forked version of Fabrice Weinberg & Ana Tudor pens. 
+ *
+ * Fabrice Weinberg
+ * https://codepen.io/FWeinb/pen/ekxmL
+ *
+ * Ana Tudor
+ * https://codepen.io/thebabydino/pen/qkDLp
+ *
+ *
+ * Vote on Reddit
+ * http://www.reddit.com/r/web_design/comments/1l6i1d/playing_with_the_random_scss_function_on
+ *
+ *
+ * Vote on HackerNews
+ * https://news.ycombinator.com/item?id=6283092
+ *
+ * 
+ * 
+ * 2013 by Tim Pietrusky
+ * timpietrusky.com
+ */
